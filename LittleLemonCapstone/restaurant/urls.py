3e766afa4ae50cacc7f3d2b4db321192306a1105
AlexHarmon2282 <aharmon@moxiepestcontrol.com>
2023-06-14 16:25:15 -0600

    Revert "Setup serializers on the menu API, possibly unsuccessfully?"
    This reverts commit 79bea59d326d16bfdc4883d93d3b7e48558f5c47.

--- LittleLemonCapstone/restaurant/urls.py
+++ LittleLemonCapstone/restaurant/urls.py
@@ -4,11 +4,7 @@ from . import views
   
 urlpatterns = [ 
     path('', views.index, name='index'),
-<<<<<<< HEAD
-    path('menu/', views.MenuItemView.as_view()),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
-=======
     path('menu/', views.menuView.as_view()),
     path('booking/', views.bookingView.as_view)
->>>>>>> parent of 79bea59 (Setup serializers on the menu API, possibly unsuccessfully?)
 ]
--- LittleLemonCapstone/restaurant/views.py
+++ LittleLemonCapstone/restaurant/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-<<<<<<< HEAD
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
 from rest_framework.viewsets import ModelViewSet
-=======
->>>>>>> parent of 79bea59 (Setup serializers on the menu API, possibly unsuccessfully?)
 from . import models
 from . import serializers
 from rest_framework.decorators import api_view
@@ -18,22 +15,17 @@ class BookingViewSet(ModelViewSet):
         items = models.booking.objects.all()
         serializer_class = serializers.BookingSerializer(items, many = True)
         return Response(serializer_class.data)
-    
-<<<<<<< HEAD
 
 class MenuItemView(ListCreateAPIView):
     queryset = models.Menu.objects.all()
     serializer_class = serializers.MenuItemSerializer
 
-=======
 class menuView(APIView):
->>>>>>> parent of 79bea59 (Setup serializers on the menu API, possibly unsuccessfully?)
     def post(self, request):
         serializer = serializers.MenuSerializer(data=request.data)
         
         if serializer.is_valid():
             serializer.save()
-<<<<<<< HEAD
             return Response({"status":"success", "data": serializer.data})
         
     def get(self, request):
@@ -49,6 +41,3 @@ class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
         pass
     def delete(self, request):
         pass
-=======
-            return Response({"status":"success", "data": serializer.data})
->>>>>>> parent of 79bea59 (Setup serializers on the menu API, possibly unsuccessfully?)
