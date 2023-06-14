edca39483a1c9cc3954fbbd38fd4c8dc12d6ad28
Alex <aharmon@moxiepestcontrol.com>
2023-06-14 14:28:09 -0600

    Rest_framework installed

--- LittleLemonCapstone/restaurant/urls.py
+++ LittleLemonCapstone/restaurant/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
   
 urlpatterns = [ 
-    path('', views.index, name='index'), 
+    path('', views.index, name='index'),
+    path('menu/', views.menuView.as_view()),
+    path('booking/', views.bookingView.as_view)
 ]
--- LittleLemonCapstone/restaurant/views.py
+++ LittleLemonCapstone/restaurant/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from . import models
+from . import serializers
 
 def index(request):
  return render(request, 'index.html', {})
+
+class bookingView(APIView):
+    def get(self, request):
+        items = models.booking.objects.all()
+        serializer = serializers.BookingSerializer(items, many = True)
+        return Response(serializer.data)
+    
+class menuView(APIView):
+    def post(self, request):
+        serializer = serializers.MenuSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status":"success", "data": serializer.data})
