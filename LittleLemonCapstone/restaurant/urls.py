31ea0a6591b39e1cf9593601aac2e560eff157eb
AlexHarmon2282 <aharmon@moxiepestcontrol.com>
2023-06-19 18:24:33 -0600

    Added djoser

--- LittleLemonCapstone/restaurant/urls.py
+++ LittleLemonCapstone/restaurant/urls.py
@@ -1,4 +1,3 @@
-from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import path 
 from . import views
   
--- LittleLemonCapstone/restaurant/views.py
+++ LittleLemonCapstone/restaurant/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from . import models
 from . import serializers
-from rest_framework.decorators import api_view, permission_classes
+
 
 def index(request):
  return render(request, 'index.html', {})
