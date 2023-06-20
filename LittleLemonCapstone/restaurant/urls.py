15ba926ef7c33be23414706d27e9f5ceadfe9056
AlexHarmon2282 <aharmon@moxiepestcontrol.com>
2023-06-19 18:07:52 -0600

    Added djoser

--- LittleLemonCapstone/restaurant/urls.py
+++ LittleLemonCapstone/restaurant/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('menu/', views.menuView.as_view()),
     path('menu_item/<int:pk>/', views.menuItemView.as_view()),
     path('booking/', views.bookingView.as_view()),
-    path('api-token-auth/', obtain_auth_token),
 ]
